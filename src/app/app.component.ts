97a2fe468f8cec91bb9f3fb3822f402dd839a47f
Valero Vanegas - Juan David <juan.valero.9c@gmail.com>
2024-04-30 10:19:11 -0500

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root',//llamado al componente, llamando al html y luego al css
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
